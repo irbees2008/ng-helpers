8633287b1b016ae138d87972306447f3262444a2
Рустам <rusiq@list.ru>
2020-04-16 22:05:03 +1000

    Перечень основных файлов

---
+++ src/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Plugins;
+
+// Базовые расширения PHP.
+
+/**
+ * Коллекция вспомогательных функций для плагинов системы NGCMS.
+ *
+ * @version: 0.0.1 от 2020-04-16
+ * @author: https://github.com/russsiq
+ *
+ */
